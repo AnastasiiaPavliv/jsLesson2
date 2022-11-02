29023aade0f8c22de11df8dec95a1c6c3da91f6e
AnastasiiaPavliv <pavlivnastia@gmail.com>
2022-11-02 23:46:04 +0200

    MASS

---
+++ script.js
@@ -0,0 +1,62 @@
+//- Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
+let arr=[1, 'two', false, 'zero', 3, 1, 'two', false, 'zero', 3];
+console.log(arr)
+
+//- Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
+let mass1={
+    title: 'asd',
+    pageCount:330,
+    genre:'novel',
+};
+console.log(mass1);
+let mass2={
+    title: 'asd',
+    pageCount:330,
+    genre:'novel',
+};
+console.log(mass2);
+let mass3={
+    title: 'asd',
+    pageCount:330,
+    genre:'novel',
+};
+console.log(mass3);
+
+//- Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors. Поле "автори" - масив. Кожен автор має поля name,age
+let m1={
+    title:'asd',
+    pageCount:330,
+    genre:'novel',
+    author:{name:'RRR', age:39},
+};
+console.log(m1);
+let m2={
+    title:'asd',
+    pageCount:330,
+    genre:'novel',
+    author:{name:'RRR', age:39},
+};
+console.log(m2);
+let m3={
+    title:'asd',
+    pageCount:330,
+    genre:'novel',
+    author:{name:'RRR', age:39},
+};
+console.log(m3);
+
+//- Створити масив з 10 об'єктами які описують сутніть "користувач". Поля: name, username,password. Вивести в консоль пароль кожного користувача
+let m10=[
+user1={name:'A',username:'T', password:111},
+user2={name:'B',username:'Y', password:222},
+user3={name:'C',username:'U', password:333},
+user4={name:'E',username:'I', password:444},
+user5={name:'F',username:'O', password:555},
+user6={name:'G',username:'P', password:666},
+user7={name:'D',username:'L', password:777},
+user8={name:'W',username:'K', password:888},
+user9={name:'Q',username:'J', password:999},
+user10={name:'R',username:'H', password:101},
+
+]
+console.log(m10);
